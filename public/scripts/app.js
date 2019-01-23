a0ba9f8df4a7a8b22a6b6b0145585ea2231f4dd1
Hilary <watson.hilary@gmail.com>
2019-01-23 17:43:41 -0500

    Added the ability to hide and show compose via button top right, we ensured it was not accessible to be hijacked

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -53,55 +53,87 @@ const data = [
 $(document).ready(function() {
 
 
-
 function renderTweets(tweets) {
-
- var $tweet = $("<article>").addClass("tweet");
-
-tweets.forEach(tweet => createTweetElement(tweet));
-
+  var $tweet = $("<article>").addClass("tweet");
+  tweets.forEach(tweet => createTweetElement(tweet));
 }
-
+//creates new tweets with the correct information
   function createTweetElement(tweet){
-
+// date calculation for when the date was first created
     var $days = `${tweet.created_at}`;
-
-  $('main.container .tweet-container').append(`
-    <article>
-      <header>
-        <div>
-        <img class="avatar" src=${tweet.user.avatars.small} >
-        <h3 class="username" >${tweet.user.name}</h3>
-      </div>
-        <a class="handler">${tweet.user.handle}</a>
-      </header>
-      <body>
-      <a class="old-tweet">${tweet.content.text}</a>
-      </body>
-      <footer>
-        <a>${$days}</a>
-        <span class="icons">
-        <i class="fas fa-flag" ></i>
-        <i class="fas fa-retweet" ></i>
-        <i class="fas fa-heart" ></i>
-      </span>
-      </footer>
-    </article>
-    `);
-
+    $('main.container .tweet-container').prepend(`
+      <article>
+        <header>
+          <div>
+          <img class="avatar" src=${tweet.user.avatars.small} >
+          <h3 class="username" >${tweet.user.name}</h3>
+        </div>
+          <a class="handler">${tweet.user.handle}</a>
+        </header>
+        <body>
+        <a class="old-tweet">${tweet.content.text}</a>
+        </body>
+        <footer>
+          <a>${$days}</a>
+          <span class="icons">
+          <i class="fas fa-flag" ></i>
+          <i class="fas fa-retweet" ></i>
+          <i class="fas fa-heart" ></i>
+        </span>
+        </footer>
+      </article>
+      `);
 };
-
+// Blocks form submission - in case of too short or too long
 $(function(){
  $("form").submit(function(event){
-    alert("This has been blocked");
-        event.preventDefault();
-    });
-
-
-  renderTweets(data);
+  event.preventDefault();
+  let stringData = $(this).serialize();
+  if(stringData.length <= 5){
+  alert("Please enter a tweet!");
+  } if(stringData.length >= 145 ){
+  alert("Your tweet is too long!");
+  }
+  $.ajax("/tweets", {data: stringData, method: 'POST'}).then(function(requestedstring){
+  loadTweets();
+    $('.new-tweet textarea').val('');
+  })
+})
 });
 
+
+// Compose button hide & show
+
+$('#composing-hideshow').on('click', function(){
+  $('.new-tweet').slideToggle(400, function(){
+    $('.container form textarea').select()
+  })
+})
+
+// var input = document.getElementById('composing-hideshow');
+// var message = document.getElementsByClassName('compose')[0];
+// input.addEventListener('focus', function() {
+//     message.style.display = 'block';
+// });
+// input.addEventListener('focusout', function() {
+//     message.style.display = 'none';
+// });
+
+
+
+
+ ///
+function loadTweets(){
+$('.tweet-container').empty() //clears all
+$.ajax('/tweets', {method: 'GET'}).then(function(tweet){
+  renderTweets(tweet);
+  })
+}
+
+loadTweets();
+
 });
+
 
 
 
